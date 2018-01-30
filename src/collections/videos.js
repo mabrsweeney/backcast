81b83ca8df5cc61b76f5262a8398abe5699da747
mabrsweeney <mabrsweeney@gmail.com>
2018-01-29 20:22:29 -0800

    Implemented search

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -1,5 +1,25 @@
 var Videos = Backbone.Collection.extend({
 
-  model: Video
+  model: Video,
+
+  search: function() {
+    $.ajax({
+      url: 'https://www.googleapis.com/youtube/v3',
+      type: 'GET',
+      data: {
+        query: 'string to search for',
+        maxResults: 5,
+        key: 'AIzaSyAN9t518dA2FZZvjhrXtwVDWmaTq8goYNs',
+        videoEmbeddable: true
+      },
+
+      success: function(data) {
+        console.log(data);
+      },
+      error: function(data) {
+        console.log('Error: Could not fetch data -> ' + data);
+      }
+    });
+  }
 
 });
