7e9d00e8c8c09f7815e9a7e9c1071a89d9bb4772
mabrsweeney <mabrsweeney@gmail.com>
2018-01-29 17:41:07 -0800

    displays video and video list

--- src/views/app.js
+++ src/views/app.js
@@ -3,7 +3,13 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    this.videos = new Videos(window.exampleVideoData);
+    this.render();
+    this.videoPlayer = new VideoPlayerView();
+    this.videoList = new VideoListView({collection: this.videos});
+
+    this.videoPlayer.render();
+    this.videoList.render();
   },
 
 
