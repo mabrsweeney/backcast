4843b50b99381de6864730ce18cd5134c45985af
mabrsweeney <mabrsweeney@gmail.com>
2018-01-30 12:34:07 -0800

    added events

--- src/views/search.js
+++ src/views/search.js
@@ -1,6 +1,24 @@
 var SearchView = Backbone.View.extend({
 
+  events: {
+    'click button': 'doSearch',
+    'keyup input': 'handleKeyUp'
+  },
+  
+  handleKeyUp: function(e) {
+    if (e.keyCode === 13) {
+      this.doSearch();
+    }
+  },
+  
+  doSearch: function() {
+    var query = $('input').val();
+    $('input').val('');
+    this.collection.search(query);
+  },
+
   el: '.search',
+  
   render: function() {
     this.$el.html(this.template());
     return this;
