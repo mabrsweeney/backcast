2926bf126086aa615d1b13226fe37ad598272ef9
mabrsweeney <mabrsweeney@gmail.com>
2018-01-29 17:40:14 -0800

    displays single videoListEntry

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,12 +1,23 @@
 var VideoListView = Backbone.View.extend({
 
+  el: '.list',
+  initialize: function () {
+    this.listenTo(this.collection, 'sync', this.render);    
+  },
+  
 
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
+    this.collection.forEach(function(model) {
+      return new VideoListEntryView({'model': model}).render();
+    });  
     return this;
   },
+
 
   template: templateURL('src/templates/videoList.html')
 
 });
+
+
