b0989b3beae32ca04a5297313debea41716b4805
mabrsweeney <mabrsweeney@gmail.com>
2018-01-30 14:19:31 -0800

    refactored rendering

--- src/views/app.js
+++ src/views/app.js
@@ -6,19 +6,31 @@ var AppView = Backbone.View.extend({
     this.videos = new Videos();
     this.videos.search('Power Rangers');
     this.render();
-    this.search = new SearchView({collection: this.videos});
-    this.search.render();
-    this.videoList = new VideoListView({collection: this.videos, el: '.list'});
-    this.videoList.render();
-    this.videoPlayer = new VideoPlayerView({collection: this.videos});
-    this.videoPlayer.render();
-
-
   },
 
 
   render: function() {
     this.$el.html(this.template());
+    
+    this.search = new SearchView({
+      collection: this.videos, 
+      el: '.search'
+    });
+    this.search.render();
+
+    this.videoList = new VideoListView({
+      collection: this.videos, 
+      el: '.list'
+    });
+    this.videoList.render();
+    
+    this.videoPlayer = new VideoPlayerView({
+      model: this.videos.at(0), 
+      collection: this.videos, 
+      el: '.player'
+    });
+    this.videoPlayer.render();
+    
     return this;
   },
 
