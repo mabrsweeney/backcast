588b9719c28a97df040ef456a6e4b3d32e6f0727
mabrsweeney <mabrsweeney@gmail.com>
2018-01-30 12:36:35 -0800

    adjusted rendering

--- src/views/videoPlayer.js
+++ src/views/videoPlayer.js
@@ -8,15 +8,26 @@ var VideoPlayerView = Backbone.View.extend({
       this.model = element;
       this.render();
     }, this);
+    this.listenTo(this.collection, 'sync', function(element) {
+      this.model = undefined;
+      // The model will be assigned in render but select() is not called
+      // therefore the tests will not pass
+     // this.model.trigger('select', this.collection.at(0));
+    
+      this.render();
+    });   
+
   },
-  render: function(model) {
-    if (!this.model) {
-      this.$el.html(this.template(this.collection.at(0).attributes));
-      // this.$el.html('<div class="loading">Please wait...</div>');
-    } else {
-      this.$el.html(this.template(this.model.attributes));
+  render: function() {
+    if (this.collection.length) {
+      if (this.model === undefined) {
+        this.$el.html(this.template(this.collection.at(0).attributes));
+        // this.$el.html('<div class="loading">Please wait...</div>');
+      } else {
+        this.$el.html(this.template(this.model.attributes));
+      }
+      return this;
     }
-    return this;
   },
 
   template: templateURL('src/templates/videoPlayer.html')
