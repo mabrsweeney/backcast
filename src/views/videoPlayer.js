bc2f7010a417fbedcd79645b175dfb71b953d540
mabrsweeney <mabrsweeney@gmail.com>
2018-01-30 14:20:11 -0800

    Refactored; used select correctly

--- src/views/videoPlayer.js
+++ src/views/videoPlayer.js
@@ -1,33 +1,29 @@
 var VideoPlayerView = Backbone.View.extend({
 
-  el: '.player',
-
   initialize: function() {
-    // this.listenTo(this.collection, 'select', this.render);
+    
     this.collection.on('select', function(element) {
       this.model = element;
+      this.$el.html('<div class="loading">Please wait...</div>');
       this.render();
     }, this);
-    this.listenTo(this.collection, 'sync', function(element) {
-      this.model = undefined;
-      // The model will be assigned in render but select() is not called
-      // therefore the tests will not pass
-     // this.model.trigger('select', this.collection.at(0));
     
+    this.listenTo(this.collection, 'sync', function(element) {
+      this.model = this.collection.at(0);
+      this.model.select();
+      this.$el.html('<div class="loading">Please wait...</div>');
       this.render();
     });   
-
   },
+  
   render: function() {
+    
     if (this.collection.length) {
-      if (this.model === undefined) {
-        this.$el.html(this.template(this.collection.at(0).attributes));
-        // this.$el.html('<div class="loading">Please wait...</div>');
-      } else {
-        this.$el.html(this.template(this.model.attributes));
-      }
-      return this;
+      this.$el.html(this.template(this.model.attributes));
+    } else {
+      this.$el.html('<div class="loading">Please wait...</div>');
     }
+    return this;
   },
 
   template: templateURL('src/templates/videoPlayer.html')
